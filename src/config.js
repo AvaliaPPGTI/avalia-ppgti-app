96782b51b0e899cc5d0bea3daf40056482151f80
tscientist <cdsevangelista@inf.ufpel.edu.br>
2025-05-29 01:00:22 -0300

    Avaliar usuarios funcionando.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
 
+//TODO: tornar o id do processo dinamico
 export const API_ENDPOINTS = {
-    RESEARCH_TOPICS: `${BASE_URL}/api/research-topics/by-process/1`,
+    RESEARCH_TOPICS: `${BASE_URL}/api/research-topics/by-process/3`,
     HOMOLOGATED_CANDIDATES_BY_TOPIC: (topicId) =>
         `${BASE_URL}/api/applications/homologated-candidates/by-research-topic/${topicId}`,
 };
